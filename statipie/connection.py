a1bead657234df0e10bdf96a189cf8122b71667a
Kamil Solecki <kamsolecki@gmail.com>
2019-12-30 17:05:33 +0100

    Read request data till a blank line

--- statipie/connection.py
+++ statipie/connection.py
@@ -1,11 +1,18 @@
+def ends_with_empty_line(data):
+    return data[-4:] == bytes("\r\n\r\n", "utf-8")
+
+
 class Connection():
     def __init__(self, conn):
         self.conn = conn
 
     def handle(self):
         with self.conn:
+            raw_request = bytes(0)
             while True:
-                data = self.conn.recv(1024)
-                if not data:
+                buf = self.conn.recv(1024)
+                raw_request += buf
+                if ends_with_empty_line(raw_request):
                     break
-                self.conn.sendall(data)
+
+            self.conn.sendall(raw_request)
